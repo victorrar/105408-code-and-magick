60bbac74f3b90cac413fd40b43807340fb1bbb16
victorrar <victorrar@mail.ru>
2016-08-22 19:31:25 +0300

    Добавлена функция вывода комментариев

--- src/reviews.js
+++ src/reviews.js
@@ -10,9 +10,74 @@ function requestRewiews(src, cb) {
   elem.src = src;
   document.head.appendChild(elem);
 }
-var reviews;
-function save(data) {
-  reviews = data;
+
+function initializeReviews() {
+  var reviews;
+  var elementToClone;
+  var container = document.querySelector('.reviews-list');
+  var template = document.querySelector('#review-template');
+  if ('content' in template) {
+    elementToClone = template.content.querySelector('.review');
+  } else {
+    elementToClone = template.querySelector('.review');
+  }
+
+  function save(data) {
+    reviews = data;
+
+    reviews.forEach(function(reviewObject) {
+      var element = elementToClone.cloneNode(true);
+      //отзыв
+      element.querySelector('.review-text').textContent = reviewObject.description;
+      //рейтинг
+      var star = element.querySelector('.review-rating');
+      var rating = reviewObject.rating;
+      var starcount;
+      switch (rating) {
+        case 2:
+          starcount = 'review-rating-two';
+          break;
+        case 3:
+          starcount = 'review-rating-three';
+          break;
+        case 4:
+          starcount = 'review-rating-four';
+          break;
+        case 5:
+          starcount = 'review-rating-five';
+          break;
+      }
+      if (starcount) {
+        star.classList.add(starcount);
+      }
+      //фото
+      var photo = new Image(124, 124);
+      var templatePhoto = element.querySelector('.review-author');
+      photo.onload = function() {
+        clearTimeout(photoTimeout);
+        templatePhoto.src = photo.src;
+        templatePhoto.setAttribute('width', '124px');
+        templatePhoto.setAttribute('heigth', '124px');
+      };
+      photo.onerror = function() {
+        element.classList.add('review-load-failure');
+      };
+      var PHOTO_TIMEOUT = 10000;
+      var photoTimeout = setTimeout(function() {
+        photo.src = '';
+        element.classList.add('review-load-failure');
+      }, PHOTO_TIMEOUT);
+      photo.src = reviewObject.author.picture;
+
+      container.appendChild(element);
+    });
+  }
+
+  var requestPath = 'http://localhost:1506/api/reviews?callback=' + callbackName;
+  var filter = document.querySelector('.reviews-filter');
+  filter.classList.add('invisible');
+  requestRewiews(requestPath, save);
+  filter.classList.remove('invisible');
+
 }
-var requestPath = 'http://localhost:1506/api/reviews?callback=' + callbackName;
-requestRewiews(requestPath, save);
+initializeReviews();
